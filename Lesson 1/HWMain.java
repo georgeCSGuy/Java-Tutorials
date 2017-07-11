6dfb9ae5c232179b8b0a0ebd6996911081b76249
georgeCSGuy <georgeccooper@pc-02123.apartment.local>
2017-07-11 23:17:12 +0200

    First Commit

---
+++ Lesson 1/HWMain.java
@@ -0,0 +1,11 @@
+/*Lesson 1, Hello World
+ * This is a simple program that prints a short phrase to the console.
+ */
+public class HWMain {
+
+	public static void main(String[] args)
+	{	
+		System.out.println("Hello World");
+	}
+
+}
